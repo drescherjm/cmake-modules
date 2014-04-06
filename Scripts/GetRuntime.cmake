f421af16b085029b067f6eb642b9d073e5e55de9
John M. Drescher <drescherjm@gmail.com>
2014-04-06 12:59:55 +0000

    Update GetRuntime.cmake using code from LungAnalysis.

--- Scripts/GetRuntime.cmake
+++ Scripts/GetRuntime.cmake
@@ -1,23 +1,45 @@
 option (GET_RUNTIME			"Create a target that will get the runtime" ON)
 
-IF (GET_RUNTIME) 
+if (GET_RUNTIME) 
+
+	SET (${PROJECT_NAME}_PACKAGING_RUNTIME "")
 
 	SET (RUNTIME_BATCH_FILENAME "${PROJECT_BINARY_DIR}/GetRuntime.bat" CACHE FILEPATH "${PROJECT_BINARY_DIR}/GetRuntime.bat")
 	
 	add_custom_target(GetRuntime  "${RUNTIME_BATCH_FILENAME}")
+	
+#########################################################################################
 
-	macro( add_runtime_file BatchFileName RuntimeFile Release )
+	macro( add_runtime_file BatchFileName RuntimeFile Configuration )
 
-		GET_FILENAME_COMPONENT( BatchFile ${BatchFileName} NAME_WE )
+		# Test if this is a valid configuration.
+		STRING( REGEX MATCH ${Configuration} __MATCHED__ ${CMAKE_CONFIGURATION_TYPES})
+		if ( NOT "${__MATCHED__}" STREQUAL "" )
 		
-		#The following will truncate the file on the first call to add_runtime_file.
-		if ( NOT DEFINED __add_runtime_file_${BatchFile}__ ) 
-			set ( __add_runtime_file_${BatchFile}__ 1)
-			file( WRITE ${BatchFileName} "REM This file will copy the runtimes to the Debug and Release binary folders\n" )
-		endif ( NOT DEFINED __add_runtime_file_${BatchFile}__)
+			if (NOT EXISTS "${EXECUTABLE_OUTPUT_PATH}/${Configuration}")
+				file( MAKE_DIRECTORY "${EXECUTABLE_OUTPUT_PATH}/${Configuration}" )
+			endif (NOT EXISTS "${EXECUTABLE_OUTPUT_PATH}/${Configuration}")
 		
-		#The following line will add the entry in the batch file for copying the Runtime file to the folder ${PROJECT_BINARY_DIR}/${Release}/
-		file( APPEND ${BatchFileName} "\"${CMAKE_COMMAND}\" -E copy_if_different \"${RuntimeFile}\" \"${EXECUTABLE_OUTPUT_PATH}/${Release}/\"\n" )
-		
+			GET_FILENAME_COMPONENT( BatchFile ${BatchFileName} NAME_WE )
+			
+			#The following will truncate the file on the first call to add_runtime_file.
+			if ( NOT DEFINED __add_runtime_file_${BatchFile}__ ) 
+				set ( __add_runtime_file_${BatchFile}__ 1)
+				file( WRITE ${BatchFileName} "REM This file will copy the runtimes to the Debug and Release binary folders\n" )
+			endif ( NOT DEFINED __add_runtime_file_${BatchFile}__)
+			
+			#The following line will add the entry in the batch file for copying the Runtime file to the folder ${PROJECT_BINARY_DIR}/${Configuration}/
+			file( APPEND ${BatchFileName} "\"${CMAKE_COMMAND}\" -E copy_if_different \"${RuntimeFile}\" \"${EXECUTABLE_OUTPUT_PATH}/${Configuration}/\"\n" )
+		endif(  NOT "${__MATCHED__}" STREQUAL "" )
 	endmacro( add_runtime_file )
-ENDIF(GET_RUNTIME)
+	
+#########################################################################################
+
+	macro( add_runtime_file_for_packaging RuntimeFile )
+	    message( STATUS "Packaging File: " ${RuntimeFile} )
+		add_runtime_file( ${RUNTIME_BATCH_FILENAME} ${RuntimeFile} Release )
+		add_runtime_file( ${RUNTIME_BATCH_FILENAME} ${RuntimeFile} RelWithDebInfo )
+		SET( ${PROJECT_NAME}_PACKAGING_RUNTIME  ${${PROJECT_NAME}_PACKAGING_RUNTIME} ${RuntimeFile} )
+	endmacro( add_runtime_file_for_packaging )
+	
+endif(GET_RUNTIME)
