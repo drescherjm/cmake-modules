9745a774f60e23921f05e1e6ddab2f311579dffe
John M. Drescher <drescherjm@gmail.com>
2015-08-17 02:20:44 +0000

    Add NSIS scripts to cmake-modules.

---
+++ Scripts/NSISMacros.cmake
@@ -0,0 +1,62 @@
+# This file contains macros and functions that help with the CPackNSIS module.
+
+#########################################################################################
+
+macro( NSIS_CREATE_DIRECTORY FolderPath )
+
+	string( REPLACE "\\" "\\\\" FolderPathNew "${FolderPath}" )
+	
+	message( STATUS FolderPathNew="${FolderPathNew}" )
+
+	# Create extra start menu folder for Admin tasks 
+	set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS 
+		${CPACK_NSIS_EXTRA_INSTALL_COMMANDS}
+		"\\n  CreateDirectory \\\"${FolderPathNew}\\\" "
+	)
+	
+	# Uninstall remove extra start menu folder for Admin tasks 
+	set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS 
+		"\\n  RMDir \\\"${FolderPathNew}\\\" "
+		${CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS}
+	)
+	
+endmacro( NSIS_CREATE_DIRECTORY )
+
+#########################################################################################
+
+macro( NSIS_CREATE_SHORTCUT ShortCutName ShortCutTarget )
+
+	string( REPLACE "\\" "\\\\" ShortCutNameNew "${ShortCutName}" )
+	string( REPLACE "\\" "\\\\" ShortCutTargetNew "${ShortCutTarget}" )
+		
+	set(result)
+	foreach(VALUE ${ARGN})
+		string( REPLACE "\\" "\\\\" VALUENew "${VALUE}" )
+		if ( "${result}" STREQUAL "" )
+			set(result "\\n  CreateShortCut \\\"${ShortCutNameNew}\\\" \\\"${ShortCutTargetNew}\\\" \\\"${VALUENew}\\\" ")
+		else()
+			set(result "${result} \\\"${VALUENew}\\\" ")
+		endif()
+	endforeach(VALUE)
+	
+	#message( STATUS ARGC=${ARGC} )
+	
+	if ( ${ARGC} LESS 3 )
+		set(result "\\n  CreateShortCut \\\"${ShortCutNameNew}\\\" \\\"${ShortCutTargetNew}\\\" ")
+	endif (  ${ARGC} LESS 3 )
+	
+	# Create extra start menu folder for Admin tasks 
+	set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS 
+		${CPACK_NSIS_EXTRA_INSTALL_COMMANDS}
+		"${result}"
+	)
+
+	# Uninstall remove extra start menu folder for Admin tasks 
+	set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS 
+		"\\n  Delete \\\"${ShortCutNameNew}\\\" " 
+		${CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS}
+	)
+
+endmacro( NSIS_CREATE_SHORTCUT )
+
+#########################################################################################
