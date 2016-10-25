dbb95f1d089b2d3dddc3d17c1e2b1b91e2a2de10
John M. Drescher <drescherjm@gmail.com>
2016-10-25 01:32:14 +0000

    Add support for cmdow.

---
+++ Scripts/SupportCMDOW.cmake
@@ -0,0 +1,14 @@
+if(WIN32)
+	option(USE_CMDOW_TO_MANIPULATE_NETSERVER_WINDOWS "Use cmdow with the network server" ON)
+	
+	if ( USE_CMDOW_TO_MANIPULATE_NETSERVER_WINDOWS ) 
+		
+		FIND_PROGRAM(CMDOW_EXECUTABLE NAME cmdow.exe PATHS C:/Utils/Other )
+
+		if ( (${CMDOW_EXECUTABLE} MATCHES "RUNJOBS_EXECUTABLE_NOTFOUND") OR (NOT EXISTS ${CMDOW_EXECUTABLE}) )
+			message( FATAL_ERROR "Please set the location of the cmdow executable or disable the cmdow feature." )
+		endif ( (${CMDOW_EXECUTABLE} MATCHES "RUNJOBS_EXECUTABLE_NOTFOUND") OR (NOT EXISTS ${CMDOW_EXECUTABLE}))
+		
+	endif( USE_CMDOW_TO_MANIPULATE_NETSERVER_WINDOWS )
+	
+endif(WIN32)
