afae1db1ee514c5cc40c79417714c55869c34322
John M. Drescher <drescherjm@gmail.com>
2019-09-23 20:42:57 +0000

    BUG_FIX: With the VCPKG built GDCM the gdcmexpat library is not created. To fix I check for the existence of  imported targets and only add the targets that exist.

--- Scripts/Support_GDCM.cmake
+++ Scripts/Support_GDCM.cmake
@@ -5,11 +5,31 @@ if (NOT GDCM_VERSION)
 endif(NOT GDCM_VERSION)
 
 if (GDCM_VERSION) 
-	if (${GDCM_VERSION} VERSION_GREATER "2.0.18") 
+    if (${GDCM_VERSION} VERSION_GREATER_EQUAL "3.0.0") 
+        # Only include the GDCM Targets that exist!
+	
+		set(_targetsDefined)
+		set(_targetsNotDefined)
+		set(_expectedTargets)
+		foreach(_expectedTarget gdcmCommon gdcmDICT gdcmDSED gdcmIOD gdcmMSFF gdcmMEXD gdcmjpeg8 gdcmjpeg12 gdcmjpeg16 gdcmcharls socketxx vtkgdcm)
+		  list(APPEND _expectedTargets ${_expectedTarget})
+		  if(NOT TARGET ${_expectedTarget})
+			list(APPEND _targetsNotDefined ${_expectedTarget})
+		  endif()
+		  if(TARGET ${_expectedTarget})
+			list(APPEND _targetsDefined ${_expectedTarget})
+		  endif()
+		endforeach()
+		
+		SET(UPMC_EXTERNAL_LIBS ${UPMC_EXTERNAL_LIBS} ${_targetsDefined})
+		
+    elseif (${GDCM_VERSION} VERSION_GREATER "2.0.18") 
 		SET(UPMC_EXTERNAL_LIBS ${UPMC_EXTERNAL_LIBS} 
 			gdcmdict gdcmCommon gdcmcharls gdcmDSED gdcmIOD 
 			gdcmexpat gdcmjpeg8 gdcmjpeg12 gdcmjpeg16 gdcmopenjpeg
 			gdcmMSFF gdcmMEXD gdcmzlib
 		)
-	endif(${GDCM_VERSION} VERSION_GREATER "2.0.18")
+	endif()
 endif(GDCM_VERSION)
+
+dump_all_variables_starting_with(GDCM)
