94c1cda00a898cad6f598300614ef50383fc29d6
John M. Drescher <drescherjm@gmail.com>
2020-03-31 15:08:41 +0000

    Update System Runtime

--- Scripts/PackageSystemRuntime.cmake
+++ Scripts/PackageSystemRuntime.cmake
@@ -59,6 +59,9 @@ macro ( PackageSystemRuntime Component )
 			
 	    find_program(MSVC_REDIST NAMES vcredist_${CMAKE_MSVC_ARCH}.exe
 		  PATHS
+		  "$ENV{ProgramW6432}/Microsoft Visual Studio/2019/Community/VC/Redist/MSVC/14.25.28508/"
+		  "$ENV{PROGRAMFILES}/Microsoft Visual Studio/2019/Community/VC/Redist/MSVC/14.25.28508/"
+		  "$ENV{${MY_PFX86}}/Microsoft Visual Studio/2019/Community/VC/Redist/MSVC/14.25.28508/"
 		  "$ENV{ProgramW6432}/Microsoft Visual Studio/2019/Community/VC/Redist/MSVC/14.23.27820/"
 		  "$ENV{PROGRAMFILES}/Microsoft Visual Studio/2019/Community/VC/Redist/MSVC/14.23.27820/"
 		  "$ENV{${MY_PFX86}}/Microsoft Visual Studio/2019/Community/VC/Redist/MSVC/14.23.27820/"
