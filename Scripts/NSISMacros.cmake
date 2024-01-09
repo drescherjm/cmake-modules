3a00558fb2a6dfde0548ff9cbcbf1782439de631
John M. Drescher <drescherjm@gmail.com>
2024-01-08 20:52:08 -0500

    Update NSISMacros.cmake
    Create a NSIS_CREATE_SHORTCUT_NO_EXTRA_ESCAPING  macro that removes some of the extra escaping of the paths for the shortcut.

--- Scripts/NSISMacros.cmake
+++ Scripts/NSISMacros.cmake
@@ -60,3 +60,40 @@ macro( NSIS_CREATE_SHORTCUT ShortCutName ShortCutTarget )
 endmacro( NSIS_CREATE_SHORTCUT )
 
 #########################################################################################
+
+macro( NSIS_CREATE_SHORTCUT_NO_EXTRA_ESCAPING ShortCutName ShortCutTargetNew )
+
+	string( REPLACE "\\" "\\\\" ShortCutNameNew "${ShortCutName}" )
+	#string( REPLACE "\\" "\\\\" ShortCutTargetNew "${ShortCutTarget}" )
+		
+	set(result)
+	foreach(VALUENew ${ARGN})
+		#string( REPLACE "\\" "\\\\" VALUENew "${VALUE}" )
+		if ( "${result}" STREQUAL "" )
+			set(result "\\n  CreateShortCut \\\"${ShortCutNameNew}\\\" \\\"${ShortCutTargetNew}\\\" \\\"${VALUENew}\\\" ")
+		else()
+			set(result "${result} \\\"${VALUENew}\\\" ")
+		endif()
+	endforeach(VALUENew)
+	
+	#message( STATUS ARGC=${ARGC} )
+	
+	if ( ${ARGC} LESS 3 )
+		set(result "\\n  CreateShortCut \\\"${ShortCutNameNew}\\\" \\\"${ShortCutTargetNew}\\\" ")
+	endif (  ${ARGC} LESS 3 )
+	
+	# Create extra start menu folder for Admin tasks 
+	set(CPACK_NSIS_EXTRA_INSTALL_COMMANDS 
+		${CPACK_NSIS_EXTRA_INSTALL_COMMANDS}
+		"${result}"
+	)
+
+	# Uninstall remove extra start menu folder for Admin tasks 
+	set(CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS 
+		"\\n  Delete \\\"${ShortCutNameNew}\\\" " 
+		${CPACK_NSIS_EXTRA_UNINSTALL_COMMANDS}
+	)
+
+endmacro( NSIS_CREATE_SHORTCUT_NO_EXTRA_ESCAPING )
+
+#########################################################################################
