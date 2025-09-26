bdd48bba0691d542fbf5e74e74b21af7b2ba8e09
John M. Drescher <drescherjm@gmail.com>
2025-09-26 11:18:09 -0400

    Begin to make these scripts installable using vcpkg in manifest mode

---
+++ cmake/cmake-modulesConfig.cmake
@@ -0,0 +1,2 @@
+# cmake/cmake-modulesConfig.cmake
+include("${CMAKE_CURRENT_LIST_DIR}/cmake-modulesTargets.cmake")
---
+++ cmake/cmake-modulesTargets.cmake
@@ -0,0 +1,2 @@
+set(cmake-modules_MODULE_DIR "${CMAKE_CURRENT_LIST_DIR}/../Modules")
+set(cmake-modules_SCRIPTS_DIR "${CMAKE_CURRENT_LIST_DIR}/../Scripts")
