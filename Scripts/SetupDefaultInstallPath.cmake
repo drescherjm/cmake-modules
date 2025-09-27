b9d5a36fc04a2a8c1ec5b60e7ec9d7d4611c6488
John M. Drescher <drescherjm@gmail.com>
2025-09-26 22:46:42 -0400

    Disable setting the install prefix when we are a port.

--- Scripts/SetupDefaultInstallPath.cmake
+++ Scripts/SetupDefaultInstallPath.cmake
@@ -1,6 +1,6 @@
 #########################################################################################
 
-#IF(WIN32)
+if (NOT DEFINED VCPKG_TARGET_TRIPLET)
 	
 	# We intentionally do not install to Program Files since on Vista or above this 
 	# may not work depending on the user settings.
@@ -8,4 +8,4 @@
 	set (DEFAULT_INSTALL_PATH /${SITE_ORGANIZATION}/${CMAKE_PROJECT_NAME} CACHE STRING "Default Install Path")
 	set (CMAKE_INSTALL_PREFIX ${DEFAULT_INSTALL_PATH} CACHE STRING "Install path" FORCE)
 	
-#ENDIF(WIN32)
+endif (NOT DEFINED VCPKG_TARGET_TRIPLET)
